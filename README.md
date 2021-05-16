04b50024407bdb847cdc0be8a22f80d459dfca17
Felikso <aleksandrasniegucka@wp.pl>
2021-05-16 20:55:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Hello
+
+Project made for my portfolio: Albert Strzyżewski (at the moment still under construction)
+
+## Repo:
+
+https://gitlab.com/aiypwzqp/nasz-okraj
+
+## You can see my website here:
+
+https://heuristic-banach-4cb9e1.netlify.app/
+
+## Contact: 
+
+aiypwzqp555@gmail.com
---
+++ gatsby-config.js
@@ -0,0 +1,78 @@
+module.exports = {
+  pathPrefix: `nasz-okraj`,
+  siteMetadata: {
+    title: "projekt",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `@react-icons/all-files`,
+/*     {
+      resolve: "gatsby-omni-font-loader",
+      options: {
+        enableListener: true,
+        custom: [
+          {
+            name: ["Mulish", "MulishBold", "MulishExtraBold", "MulishSemiBold", "MulishBlack"],
+            file: "src/style/fonts.css",
+          },
+        ],
+      },
+    }, */
+/*     {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Ubuntu`
+        ],
+        display: 'swap',
+        attributes: {
+          rel: "stylesheet preload prefetch",
+        },
+      }
+    }, */
+   /*  {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Oswald`,
+            subsets: [`latin`],
+          },
+          {
+            family: `Open Sans`,
+            variants: [`400`, `700`]
+          },
+        ],
+      },
+    }, */
+    {
+      resolve: `@el7cosmos/gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: 'Roboto',
+            variants: [
+              '400',
+              '400i',
+              '700',
+              '700i',
+            ],
+            subsets: [
+              'latin-ext',
+            ],
+          },
+        ],
+      },
+    }
+    
+  ],
+};
